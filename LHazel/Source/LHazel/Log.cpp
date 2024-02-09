2edb9154428f4290d1f9cb2905fc0f8a6f4dfcc0
Easi-GH <easi.guo@outlook.com>
2024-02-10 00:23:05 +0800

    Add spdlog as vendor; Add log system; Add Premake5;

---
+++ LHazel/Source/LHazel/Log.cpp
@@ -0,0 +1,24 @@
+#include "Log.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace LHazel
+{
+
+    std::shared_ptr<spdlog::logger> Log::_CoreLogger;
+    std::shared_ptr<spdlog::logger> Log::_ClientLogger;
+
+    void Log::Init()
+    {
+        // Init logger pattern.
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+
+        // Init core logger.
+        _CoreLogger = spdlog::stdout_color_mt("LHAZEL");
+        _CoreLogger -> set_level(spdlog::level::trace);
+
+        // Init client logger.
+        _ClientLogger = spdlog::stdout_color_mt("APPLICATION");
+        _ClientLogger -> set_level(spdlog::level::trace);
+    }
+
+}
---
+++ Premake_VS2022.bat
@@ -0,0 +1,2 @@
+CALL Vendor\Premake@5.0-beta2\premake5.exe vs2022
+PAUSE
